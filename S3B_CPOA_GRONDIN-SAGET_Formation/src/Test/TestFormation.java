2ec8343c0ad9fdd40258d7a7e12d86798d1c2e76
Florent <florent.grondin2@ent.univ-lorraine.fr>
2020-10-08 16:59:33 +0200

    autre

---
+++ S3B_CPOA_GRONDIN-SAGET_Formation/src/Test/TestFormation.java
@@ -0,0 +1,27 @@
+
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Grondin Florent, Saget Louis
+ *
+ */
+public class TestFormation {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
